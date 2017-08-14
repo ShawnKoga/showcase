fc657e21fbf48d182e482845a4ca4245ff38ada9
Camille <shawn.koga@aggiemail.usu.edu>
2017-08-14 07:44:17 -0600

    added a counter for practice and fun

---
+++ src/components/Topics/Counter.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+export default class Counter extends Component {
+    constructor () {
+        super();
+
+        this.state = {
+            count: 0
+        }
+    }
+
+
+    render() {
+
+        setTimeout(() => {
+            this.setState({
+                count: this.state.count + 1
+            })
+        }, 1000)
+
+        return (
+            <div className="puzzleBox evenAndOddPB">
+                <h4>Counter</h4>
+                <span className="resultsBox">Seconds since component rendered : {this.state.count}</span>
+            </div>
+        )
+    }
+}
+
